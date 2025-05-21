db0cc84052c7b878a4d62e4cd6d8a119b8d2f1fb
satyendra chaudhary <148510277+Satyendrachaudhary143@users.noreply.github.com>
2025-05-21 12:06:44 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://urlshortner-id47.onrender.com',
         changeOrigin: true,
         secure: false
       }
