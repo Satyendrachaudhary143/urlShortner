7b3bf794d7dcebe1af64221d39a5351bafb1c7f9
satyendra_chaudhary <satyendrachaudhary38@gmail.com>
2025-05-29 10:14:45 +0530

    api change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     server: {
     proxy: {
       '/api': {
-        target: 'https://urlshortner-id47.onrender.com',
+        target: 'https://url-shortner-nine-ochre.vercel.app',
         changeOrigin: true,
         secure: false
       }
